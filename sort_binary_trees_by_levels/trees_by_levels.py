d8228e7cc6fb99d78a44f893603d4e945e9b019b
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-05-02 01:30:33 +0300

    added class node so it could work not only on codewars

--- sort_binary_trees_by_levels/trees_by_levels.py
+++ sort_binary_trees_by_levels/trees_by_levels.py
@@ -1,4 +1,11 @@
 from collections import deque
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
 def tree_by_levels(node):
     if node is None:
         return []
