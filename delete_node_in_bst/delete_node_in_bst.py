eb801fd0bc07b5471f7a11b3f8959dfaa6a47bc0
Oleksii-Lasiichuk <oleks1297@gmail.com>
2025-05-02 16:46:51 +0300

    coded last problem

--- delete_node_in_bst/delete_node_in_bst.py
+++ delete_node_in_bst/delete_node_in_bst.py
@@ -1,9 +1,29 @@
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            #it is a node to delete
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+
+            nextt = root.right
+            while nextt.left:
+                nextt = nextt.left
+            root.val = nextt.val
+            root.right = self.deleteNode(root.right, nextt.val)
         
+        return root
